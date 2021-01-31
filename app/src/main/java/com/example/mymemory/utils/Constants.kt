681631a9079b79245f0bdd132f5dc5a9b2eb5745
Robert Queeney <robert.queeney@gmail.com>
2021-01-30 20:29:57 -0600

    first commit  game working  need to finalize customization options

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_baseline_bedtime_24,
+        R.drawable.ic_baseline_flash_off_24,
+        R.drawable.ic_baseline_redeem_24,
+        R.drawable.ic_baseline_work_off_24,
+        R.drawable.ic_face,
+        R.drawable.ic_flight,
+        R.drawable.ic_flower,
+        R.drawable.ic_heart,
+        R.drawable.ic_home,
+        R.drawable.ic_school,
+        R.drawable.ic_send,
+        R.drawable.ic_star
+)
